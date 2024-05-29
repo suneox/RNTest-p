9187139a803abaef110a4f3b19dcb2f0ac72ed9a
Cong Pham <ptcong411@gmail.com>
2024-05-29 20:48:19 +0700

    handle paste image with patch

--- App.tsx
+++ App.tsx
@@ -5,91 +5,67 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
+import {TextInput} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {MarkdownTextInput} from '@expensify/react-native-live-markdown';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const [imageUri, setImageUri] = useState<string>('');
+  const [mimeType, setMimeType] = useState<string>('');
+  const handlePasteImage = (event: any) => {
+    const {uri, mime, base64Source} = event.nativeEvent;
+    setMimeType(mime);
+    if (uri) {
+      setImageUri(uri);
+    } else if (base64Source) {
+      setImageUri(base64Source);
+    }
+    console.log('___________  ___________', {uri, mime});
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={{backgroundColor: 'white', height: '100%'}}>
+      <StatusBar barStyle={'dark-content'} />
+      <ScrollView>
+        <View style={{padding: 16, gap: 16}}>
+          <TextInput
+            style={[styles.input, {borderColor: 'blue'}]}
+            placeholder="TextInput Default"
+            placeholderTextColor="gray"
+          />
+          <TextInput
+            style={[styles.input, {borderColor: 'red'}]}
+            placeholder="TextInput Enabled Paste Image"
+            placeholderTextColor="gray"
+            onPasteImage={handlePasteImage}
+          />
+          <MarkdownTextInput
+            multiline
+            style={[styles.input, {borderColor: 'green', minHeight: 100}]}
+            placeholder="Expensive Live Markdown Editor"
+            placeholderTextColor="gray"
+            onPasteImage={handlePasteImage}
+          />
+          {imageUri && (
+            <View>
+              <Text onPress={() => setImageUri('')}>Clear</Text>
+              <Text>{mimeType}</Text>
+              <Image
+                source={{uri: imageUri}}
+                style={{width: 200, height: 200, resizeMode: 'cover'}}
+              />
+            </View>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -97,21 +73,10 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    borderWidth: 1,
+    padding: 8,
+    borderColor: 'gray',
   },
 });
 
